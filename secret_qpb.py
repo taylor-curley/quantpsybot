029b65e8a352982266bf8619021b635e2ab00447
Taylor C <tmc2737@gmail.com>
2016-08-11 00:42:24 -0400

    Create secret_qpb.py

---
+++ secret_qpb.py
@@ -0,0 +1,6 @@
+
+These bots will require communication with Google and Twitter's APIs. You will have to get your own keys!
+
+For Twitter, you will need a Consumer Key, a Consumer Secret Key, an Owner ID, Access Token, and Access Token Secret.
+
+For Google's URL shortener, you will only need an API key.
